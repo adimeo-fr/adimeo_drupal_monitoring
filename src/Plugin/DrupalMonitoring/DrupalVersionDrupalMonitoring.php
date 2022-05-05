60d131b9aeb5a3526765f37a614d9dcad3ec51fc
Aimad Bachar <abachar@adimeo.com>
2022-05-05 14:01:16 +0200

    [MONITORING] add plugin for drupal version

---
+++ src/Plugin/DrupalMonitoring/DrupalVersionDrupalMonitoring.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\adimeo_drupal_monitoring\Plugin\DrupalMonitoring;
+
+use Drupal\adimeo_drupal_monitoring\Annotation\DrupalMonitoring;
+use Drupal\adimeo_drupal_monitoring\Plugin\DrupalMonitoringBase;
+use Drupal\adimeo_drupal_monitoring\Plugin\DrupalMonitoringInterface;
+
+/**
+ *  Drupal Version
+ *
+ * @DrupalMonitoring(
+ *  id = "drupal_version",
+ *  label = "Version de drupal du site"
+ * )
+ */
+class DrupalVersionDrupalMonitoring extends DrupalMonitoringBase implements DrupalMonitoringInterface
+{
+
+    /**
+     * Retrieve version of drupal site
+     *
+     * @return string
+     */
+    public function fetch()
+    {
+        $drupalVersion = \Drupal::VERSION;
+
+        return $drupalVersion;
+    }
+}
